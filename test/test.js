9443055611824980b284289ba9f5a1b05189812a
Bernard Hamlin <bernie.hamlin@glasgowsciencecentre.org>
2016-02-03 11:43:12 +0000

    initial version

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@
 var path = require('path');
 var generate = require('markdown-it-testgen');
 
-describe('markdown-it-video', function() {
+describe('markdown-it-apester', function() {
   var md = require('markdown-it')({
     html: true,
     linkify: true,
     typography: true
   }).use(require('../'));
-  generate(path.join(__dirname, 'fixtures/video.txt'), md);
+  generate(path.join(__dirname, 'fixtures/apester.txt'), md);
 });
